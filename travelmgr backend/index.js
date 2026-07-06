93b9670427de2083ab927ad171304f87b03535b3
ericvanm <eric.vanmeerbeck@gmail.com>
2026-07-06 11:43:52 +0200

    Update the IP adresse for listening port

--- travelmgr backend/index.js
+++ travelmgr backend/index.js
@@ -2,6 +2,6 @@ const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 logger.info(`Starting App`)
-app.listen(config.PORT, () => {
+app.listen(config.PORT, '0.0.0.0', () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
