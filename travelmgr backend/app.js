3d77e3b0d400345b78c453d438f4f65a3d791b6a
ericvanm <eric.vanmeerbeck@gmail.com>
2026-07-06 21:45:41 +0200

    Cors problem

--- travelmgr backend/app.js
+++ travelmgr backend/app.js
@@ -30,7 +30,11 @@ if (isProduction) {
 
 app.use(cors({
   origin(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (!origin) {
+      callback(null, true)
+      return
+    }
+    if (allowedOrigins.includes(origin) || origin.endsWith('.vercel.app') || origin.endsWith('.onrender.com')) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
